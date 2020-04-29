31b8f195a3c25b8f78ba3650cecd35a9f1b2c51d
Liam Keane <liamkeane6@yahoo.co.uk>
2020-04-30 00:37:18 +0100

    Commented the code

--- RemoveSubtitles.py
+++ RemoveSubtitles.py
@@ -1,25 +1,21 @@
+#Imports
 import cv2
 import os
 import numpy as np
 import pytesseract as pt
 from pytesseract import Output
 
-#img = cv2.imread('test.jpg')
+#Read single image
 img = cv2.imread('E:/Media/Videos/Test/frame10.jpg')
 
+#Main Process
+#Detect subtitles, create mask, inpaint that area
 mask = np.zeros(img.shape, np.uint8)
 recogImg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-#recogImg = cv2.GaussianBlur(recogImg, (3,3), 0)
-#recogImg = cv2.medianBlur(recogImg, 9)
 recogImg = cv2.threshold(recogImg, 240, 255, cv2.THRESH_BINARY)[1]
 kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5,5))
 recogImg = cv2.morphologyEx(recogImg, cv2.MORPH_CLOSE, kernel)
-#recogImg = cv2.morphologyEx(recogImg, cv2.MORPH_OPEN, kernel)
 recogImg = cv2.dilate(recogImg, kernel, iterations=3)
-#recogImg = 255 - recogImg
-#subtitlesString = pt.image_to_string(recogImg, lang='eng', config='--psm 3')
-#subtitles = pt.image_to_data(recogImg, output_type=Output.DICT)
-#n_boxes = len(subtitles['level'])
 contours, hierarchy = cv2.findContours(recogImg, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 
 if len(contours) != 0:
@@ -31,21 +27,13 @@ mask[y:y+h, x:x+w] = recogImg[y:y+h, x:x+w]
 mask = cv2.erode(mask, kernel, iterations=1)
 mask = cv2.GaussianBlur(mask, (3,3), 0)
 
-
-# for i in range(n_boxes):
-#     if i > 0:
-#         (x,y,w,h) = (subtitles['left'][i], subtitles['top'][i], subtitles['width'][i], subtitles['height'][i])
-#         mask = cv2.rectangle(mask, (x,y), (x+w, y+h), (255,255,255), -1)
-#         print(x, y)
-#         print(w, h)
-#print(subtitlesString)
-#mask = recogImg
-#
 cleanedImg = cv2.inpaint(img, mask, 3, cv2.INPAINT_TELEA)
  
-cv2.imshow("original",img)
-cv2.imshow("b&w", recogImg) 
-cv2.imshow("mask",mask)
+#Show all stages of process
+cv2.imshow("original", img)
+cv2.imshow("b&w", recogImg)
+cv2.imshow("mask", mask)
 cv2.imshow("clean", cleanedImg)
 
+#Press key to close
 cv2.waitKey(0)
--- VideoTextRemover.py
+++ VideoTextRemover.py
@@ -1,3 +1,4 @@
+#Imports
 import cv2
 import os
 import numpy as np
@@ -6,39 +7,46 @@ from pytesseract import Output
 import re 
 import time
 
-start_time = time.time()
 
+#This is the folder the video will be in, and a subfolder for the temporary processing files.
+dir = 'C:/Videos/' #Place video directory here
+
+#Sorts alphanumerically with frame formatting
 def sortedproper( l ):    
     convert = lambda text: int(text) if text.isdigit() else text 
     alphanum_key = lambda key: [ convert(c) for c in re.split('([0-9]+)', key) ] 
     return sorted(l, key = alphanum_key)
 
-#img = cv2.imread('test.jpg')
-dir = 'E:/Media/Videos/'
-#dir = 'C:/Users/liamk/ScrubtitlesFiles/'
+#Used for finding execution time
+start_time = time.time()
+
+#Video capture
 vid = cv2.VideoCapture(os.path.join(dir, 'test.mp4'))
 frame_counter = 0
 
-if not os.path.exists(os.path.join(dir,'Test')):
-    os.mkdir(os.path.join(dir,'Test'))
-    print("Directory " , os.path.join(dir,'Test') ,  " Created ")
+#Checks if Temp folder exists, if not, make one
+if not os.path.exists(os.path.join(dir,'Temp')):
+    os.mkdir(os.path.join(dir,'Temp'))
+    print("Directory " , os.path.join(dir,'Temp') ,  " Created ")
 else:    
-    print("Directory " , os.path.join(dir,'Test') ,  " already exists")
+    print("Directory " , os.path.join(dir,'Temp') ,  " already exists")
 
-os.chdir(os.path.join(dir,'Test'))
+os.chdir(os.path.join(dir,'Temp'))
 
+#Main process
+#Runs through every frame to detect subtitles
+#Saves each frame into Temp folder
 while (frame_counter < vid.get(cv2.CAP_PROP_FRAME_COUNT)):
     ret, img = vid.read()
     name = "frame%d.jpg" % (frame_counter)
+
     if not ret:
         break
-    if not os.path.exists(os.path.join(dir,"Test/frame%d.jpg" % (frame_counter))):
+
+    if not os.path.exists(os.path.join(dir,"Temp/frame%d.jpg" % (frame_counter))):
         mask = np.zeros(img.shape, np.uint8)
         recogImg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        #recogImg = cv2.threshold(recogImg, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
         recogImg = cv2.threshold(recogImg, 240, 255, cv2.THRESH_BINARY)[1]
-        #recogImg = cv2.GaussianBlur(recogImg, (3,3), 0)
-        #recogImg = cv2.medianBlur(recogImg, 9)
         kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5,5))
         recogImg = cv2.morphologyEx(recogImg, cv2.MORPH_CLOSE, kernel)
         recogImg = cv2.dilate(recogImg, kernel, iterations=3)
@@ -55,44 +63,21 @@ while (frame_counter < vid.get(cv2.CAP_PROP_FRAME_COUNT)):
 
         if len(mask.shape) > 2:  
             mask = cv2.cvtColor(mask, cv2.COLOR_BGR2GRAY)
-        #kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3,3))
-        #recogImg = cv2.morphologyEx(recogImg, cv2.MORPH_OPEN, kernel, iterations=1)
-        #recogImg = 255 - recogImg
-        #subtitlesString = pt.image_to_string(recogImg, lang='eng', config='--psm 7')
-        #subtitles = pt.image_to_data(recogImg, output_type=Output.DICT)
-        # n_boxes = len(subtitles['level'])
-        # for i in range(n_boxes):
-        #     if i > 0:
-        #         (x,y,w,h) = (subtitles['left'][i], subtitles['top'][i], subtitles['width'][i], subtitles['height'][i])
-        #         if not x == 0 or not y == 0:
-        #             mask = cv2.rectangle(mask, (x,y), (x+w, y+h), (255,255,255), -1)
-        #             print(x, y)
-        #             print(w, h)
-        #mask = recogImg
-        #mask = cv2.cvtColor(mask, cv2.COLOR_BGR2GRAY)
-        cleanedImg = cv2.inpaint(img, mask, 3, cv2.INPAINT_TELEA)
-        #cv2.imshow("original",img)
-        #cv2.imshow("clean", cleanedImg)        
+
+        cleanedImg = cv2.inpaint(img, mask, 3, cv2.INPAINT_TELEA)      
         cv2.imwrite(name, cleanedImg)
         frame_counter += 1
-        #cv2.waitKey(0)
 
+#Move back to original video directory and begin saving frames into one video
+os.chdir(dir)
 video = cv2.VideoWriter('output.avi', 0, int(vid.get(cv2.CAP_PROP_FPS)), (int(vid.get(cv2.CAP_PROP_FRAME_WIDTH)), int(vid.get(cv2.CAP_PROP_FRAME_HEIGHT))))
-images = [img for img in os.listdir(os.path.join(dir,'Test')) if img.endswith(".jpg")]
+images = [img for img in os.listdir(os.path.join(dir,'Temp')) if img.endswith(".jpg")]
 images = sortedproper(images)
 for image in images:
     print(image)
     video.write(cv2.imread(image))
 
+#Cleanup and print execution time
 video.release()
 vid.release()
 print("--- %s seconds ---" % (time.time() - start_time))
-
-#print(subtitlesString) 
-
-#cv2.imshow("b&w", recogImg) 
-#cv2.imshow("mask",mask)
-
-
-
-#mask = Text shapes from Tesseract ocr
